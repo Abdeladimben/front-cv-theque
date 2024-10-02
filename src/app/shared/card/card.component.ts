767e69e90fbd1dde166f2f4d037a641b8ca2b304
Oussama BITAOUI <oussamabitaa10@gmail.com>
2024-10-03 00:41:43 +0100

    cards modif

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { CarouselModule } from 'primeng/carousel';
+
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CardModule,ButtonModule],
+  imports: [CardModule,ButtonModule,CarouselModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+
+  @Input() title: string = '';
+  @Input() textContent: string = '';
+
+  @Input() images: string[] = []; // To accept an array of images
+
+  // Determine if there's only one image
+  get isSingleImage(): boolean {
+    return this.images.length === 1;
+  }
 
 }
